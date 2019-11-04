6b025e63062bb1f801631fe4aa647c9137be54f6
coip <e@coip.me>
2019-11-04 12:42:36 -0600

    template modifications:
    - adds OWASP Impact
    - adds PCI impact
    - adds Reversion steps
    - updates some phrasing

--- form/template.go
+++ form/template.go
@@ -4,41 +4,75 @@ const ReleaseTemplate string = `<form class="form-horizontal">
 <fieldset>
 
 <!-- Form Name -->
-<legend>RELEASE FORM</legend>
+<legend>PDS SOFTWARE RELEASE FORM</legend>
 
 <!-- Prepended text-->
 <div class="form-group">
-  <label class="col-md-4 control-label" for="prependedtext">Author</label>
+  <label class="col-md-4 control-label" for="prependedtext">Release Date:</label>
   <div class="col-md-4">
     <div class="input-group">
-      <span class="input-group-addon">prepend</span>
-      <input id="prependedtext" name="prependedtext" class="form-control" placeholder="{{ .Author }}" type="text">
-    </div>
-    <p class="help-block">help</p>
-  </div>
-</div>
-
-<!-- Prepended text-->
-<div class="form-group">
-  <label class="col-md-4 control-label" for="prependedtext">Commit</label>
-  <div class="col-md-4">
-    <div class="input-group">
-      <span class="input-group-addon">prepend</span>
-      <input id="prependedtext" name="prependedtext" class="form-control" placeholder="{{ .Commit }}" type="text">
-    </div>
-    <p class="help-block">help</p>
-  </div>
-</div>
-
-<!-- Prepended text-->
-<div class="form-group">
-  <label class="col-md-4 control-label" for="prependedtext">Release Time</label>
-  <div class="col-md-4">
-    <div class="input-group">
-      <span class="input-group-addon">prepend</span>
       <input id="prependedtext" name="prependedtext" class="form-control" placeholder="{{ .Date }}" type="text">
     </div>
-    <p class="help-block">help</p>
+  </div>
+</div>
+
+<!-- Prepended text -->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="prependedtext">Product:</label>
+  <div class="col-md-4">
+    <div class="input-group">
+      <input id="prependedtext" name="prependedtext" class="form-control" placeholder="{{ .Product }}" type="text">
+    </div>
+  </div>
+</div>
+
+<!-- Prepended text-->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="prependedtext">Included Changes:</label>
+  <div class="col-md-4">
+    <div class="input-group">
+      <input id="prependedtext" name="prependedtext" class="form-control" placeholder="{{ .Commit }}" type="text">
+    </div>
+  </div>
+</div>
+
+<!-- Prepended text -->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="prependedtext">Changes Approved By:</label>
+  <div class="col-md-4">
+    <div class="input-group">
+      <input id="prependedtext" name="prependedtext" class="form-control" placeholder="{{ .CommitterName }}" type="text">
+    </div>
+  </div>
+</div>
+
+<!-- Prepended text -->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="prependedtext">Back-Out Procedure:</label>
+  <div class="col-md-4">
+    <div class="input-group">
+      <input id="prependedtext" name="prependedtext" class="form-control" placeholder="{{ .BackOutProc }}" type="text">
+    </div>
+  </div>
+</div>
+
+<!-- Prepended text -->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="prependedtext">Impacts PCI Compliance:</label>
+  <div class="col-md-4">
+    <div class="input-group">
+      <input id="prependedtext" name="prependedtext" class="form-control" placeholder="{{ .PCIImpact }}" type="text">
+    </div>
+  </div>
+</div>
+
+<!-- Prepended text -->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="prependedtext">OWASP Impact:</label>
+  <div class="col-md-4">
+    <div class="input-group">
+      <input id="prependedtext" name="prependedtext" class="form-control" placeholder="{{ .OWASPImpact }}" type="text">
+    </div>
   </div>
 </div>
 
