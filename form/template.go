3a8117f41045a2c9479bc2979754bf431730f0ef
Stephanie Honore <stephaniehonore@Stephanies-MacBook-Pro-2.local>
2019-12-13 16:52:10 -0600

    updated html template

--- form/template.go
+++ form/template.go
@@ -3,87 +3,89 @@ package form
 //ReleaseTemplate is the go html/template used to render the form.
 //Can publish HTML, though current workflow is to send the rendered HTML payload
 //  to the athenapdf microservice for signing/distribution.
-const ReleaseTemplate string = `<form class="form-horizontal">
-<fieldset>
-
+const ReleaseTemplate string = `
+<!DOCTYPE html>
+<html>
+<head>
+<title>PDS SOFTWARE RELEASE FORM</title>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+</head>
+<body>
 <!-- Form Name -->
-<legend>PDS SOFTWARE RELEASE FORM</legend>
-
+<container>
+  <div class="row justify-content-start mt-5 text-dark">
+    <div class="col">
+      <h1 class="text-center text-dark">PDS SOFTWARE RELEASE FORM</h1>
+    </div>
+  </div>
 <!-- Prepended text-->
-<div class="form-group">
-  <label class="col-md-4 control-label" for="prependedtext">Release Date:</label>
-  <div class="col-md-4">
-    <div class="input-group">
-      <input id="prependedtext" name="prependedtext" class="form-control" placeholder="{{ .Date }}" type="text">
+   <div class="border-bottom">
+    <div class="row mt-4 mx-5">
+      <div class="col-xl-12">
+        <p class="paragraph-txt d-inline"><b>Release Date:</b> {{ .Date }}</p>
+      </div>
+    </div>
+    <div class="row mt-2 mx-5">
+      <div class="col-xl-12">
+        <!-- Prepended text -->
+        <p class="paragraph-txt d-inline"><b>Product:</b> {{ .Product }}</p>
+      </div>
+    </div>
+    </div>
+
+  <div class="row mt-3 mx-5 text-dark">
+  <div class="col">
+    <!-- Prepended text-->
+    <div>
+        <div>
+      
+        {{ range $key, $value := .Changes }}
+        <h3 class="sub-heading- my-4">Change Item {{ inc $key }}: {{ $value.Title }}</h4>
+        <p class="mb-2"><b>PR:</b> [{{ $value.ID }}]</p>
+        <p class="mb-2"><b>Commit:</b> [{{ $value.CommitSHA }}]</p>
+        <p class="paragraph-txt"><b>Written By:</b> {{ $value.Developer }}</p>
+        <p class="paragraph-txt"><b>Description of Issue:</b> {{ $value.SummaryOfChangesNeeded }}</p>
+        <p class="paragraph-txt"><b>Description of Solution:</b> {{ $value.SummaryOfChangesImplemented }}</p>
+        {{ if $value.IssueID }}
+        <p class="paragraph-txt"><b>Issue ID: </b>{{ $value.IssueID }}</p>
+        {{ end }}
+        {{ if $value.ApprovedBy }}
+        <p class="paragraph-txt"><b>Approved by:</b> {{ $value.ApprovedBy }}</p>
+        {{ end }} 
+          <hr></hr>
+        {{ end }}  
+        </div>
+
+    </div>
+
     </div>
   </div>
-</div>
+  </div>
+  <div class="row mx-5 mt-3">
+    <div class="col">
+    <h3 class="sub-heading- my-3">Additional Information</h3>
+      <!-- Prepended text -->
+      <div>
+        <p class="paragraph-txt d-inline my-2"><b>Back-Out Procedure:</b> {{ .BackOutProc }}</p>
+      </div>
 
-<!-- Prepended text -->
-<div class="form-group">
-  <label class="col-md-4 control-label" for="prependedtext">Product:</label>
-  <div class="col-md-4">
-    <div class="input-group">
-      <input id="prependedtext" name="prependedtext" class="form-control" placeholder="{{ .Product }}" type="text">
+      <!-- Prepended text -->
+      <div>
+          <p class="paragraph-txt d-inline my-2">
+          <b>Impacts PCI Compliance:</b> {{ .PCIImpact }}
+          </p>
+      </div>
+
+      <!-- Prepended text -->
+      <div>
+        <p class="paragraph-txt d-inline my-2"><b>OWASP Impact:</b> {{ .OWASPImpact }}</p>
+      </div>
     </div>
   </div>
-</div>
-
-<!-- Prepended text-->
-<div class="form-group">
-  <label class="col-md-4 control-label" for="prependedtext"><h1>Included Changes:</h1></label>
-  <div class="col-md-4">
-    <div class="input-group">
-    {{ range $key, $value := .Changes }}
-    <h2>Change Item {{ inc $key }}: {{ $value.Title }}</h2><br><b>PR:[{{ $value.ID }}] Commit:[{{ $value.CommitSHA }}]</b></li>
-    <h3>Written By:</h3>
-    <h5>{{ $value.Developer }}</h5>
-    <h3>Summary of changes:</h3>
-    <h5>Description of Issue:</h5>
-    <p>{{ $value.SummaryOfChangesNeeded }}</p>
-    <h5>Description of Solution:</h5>
-    <p>{{ $value.SummaryOfChangesImplemented }}</p>
-    {{ if $value.IssueID }}
-    <b>Issue ID:{{ $value.IssueID }}</b>
-    {{ end }}
-    {{ if $value.ApprovedBy }}
-    <b>Approved by: {{ $value.ApprovedBy }}</b>
-    {{ end }}
-    <hr><hr>
-  {{ end }}    </div>
-  </div>
-</div>
-
-<!-- Prepended text -->
-<div class="form-group">
-  <label class="col-md-4 control-label" for="prependedtext">Back-Out Procedure:</label>
-  <div class="col-md-4">
-    <div class="input-group">
-      <input id="prependedtext" name="prependedtext" class="form-control" placeholder="{{ .BackOutProc }}" type="text">
-    </div>
-  </div>
-</div>
-
-<!-- Prepended text -->
-<div class="form-group">
-  <label class="col-md-4 control-label" for="prependedtext">Impacts PCI Compliance:</label>
-  <div class="col-md-4">
-    <div class="input-group">
-      <input id="prependedtext" name="prependedtext" class="form-control" placeholder="{{ .PCIImpact }}" type="text">
-    </div>
-  </div>
-</div>
-
-<!-- Prepended text -->
-<div class="form-group">
-  <label class="col-md-4 control-label" for="prependedtext">OWASP Impact:</label>
-  <div class="col-md-4">
-    <div class="input-group">
-      <input id="prependedtext" name="prependedtext" class="form-control" placeholder="{{ .OWASPImpact }}" type="text">
-    </div>
-  </div>
-</div>
-
-</fieldset>
-</form>
+</container>
+</body>
+</html>
 `
