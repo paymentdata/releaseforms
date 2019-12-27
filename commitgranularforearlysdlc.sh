a12bcf2cdaa5e4d3d723ff41995e6dabc86f212a
ethan c <e@coip.me>
2019-12-27 16:09:50 -0600

    consumes releasespans to generate csv for each releases set of commits

--- commitgranularforearlysdlc.sh
+++ commitgranularforearlysdlc.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
-#RELEASE_SPAN takes the form "$fromSHA..$toSHA"
-RELEASE_SPAN=$1
+# given a file, pass the path to this script.
+#$ cat ~/releasehistory
+#epoch:fromSHA:toSHA
+#1545917656:6769f55:4094e17
+#1545931758:4094e17:d069bd7
+#1546116375:d069bd7:885be84
+#1546452309:885be84:8d9e98d
 
-commits=$(git log ${RELEASE_SPAN} --pretty=format:"%h" --no-merges)
-commitcount=$(wc -l <<< "$commits")
+for RELEASE_SPAN in $(awk -F: '{print $2".."$3}' $1); do
+    echo -e "\tgenerating csv for changelog in releasespan: ${RELEASE_SPAN}"
 
-for idx in $(seq 1 ${commitcount}); do
-    commit_id=$(sed "${idx}q;d" <<< "$commits")
-    git log -1 --pretty=format:%at,%h,%an,%s "${commit_id}"
+    commits=$(git log "${RELEASE_SPAN}" --pretty=format:"%h" --no-merges)
+    commitcount=$(wc -l <<< "$commits")
+
+    for idx in $(seq 1 ${commitcount}); do
+        commit_id=$(sed "${idx}q;d" <<< "$commits")
+        git log -1 --pretty=format:%at,%h,%an,%s "${commit_id}"
+    done
+
+    echo -e "\tfinished generating csv for changelog\n"
 done
